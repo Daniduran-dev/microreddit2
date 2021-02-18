6202c5ac1f813873b3591b0b40773c8d63117edd
Daniduran-dev <danigduran@gmail.com>
2021-02-18 23:07:59 +0700

    add model for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 20 }
+  validates :body, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 500 }
 
-  belongs_to :user_id
+  belongs_to :user
 end
