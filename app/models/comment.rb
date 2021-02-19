3d25240ad1d934cba07a2dbf264e0e85275d0e2c
Daniduran-dev <danigduran@gmail.com>
2021-02-19 22:28:09 +0700

    add comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   validates :author, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 20 }
   validates :body, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 200 }
-  validates :user_id
-  validates :post_id
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 
   belongs_to :user
   belongs_to :post
